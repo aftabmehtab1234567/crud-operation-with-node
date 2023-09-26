74a4989f7f4675c4b6d550c084ff16290214c8e1
aftab alam <aftabalam9008@gmail.com>
2023-09-26 07:39:44 -0400

    hi

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-<<<<<<< HEAD
-=======
-
->>>>>>> 9ccf282 (hi)
 const app = express();
 const PORT = process.env.PORT || 5000; // Using the PORT variable from the .env file
 const dbURI = process.env.Db_URI; // Using the Db_URI variable from the .env file
@@ -22,18 +18,10 @@ db.once('open', () => console.log('Connected successfully'));
   app.use(express.json());  
   app.use(session({
         secret:"my secret key",
-<<<<<<< HEAD
         saveUninitialized:true,
         resave:false,
     }),
   );
-=======
-        saveUninitialized:false,
-        resave:false,
-    }),
-  );
- 
->>>>>>> 9ccf282 (hi)
 app.get("/" ,(req ,res,next)=>{
     res.locals.message=req.session.message;
     delete req.session.message;
@@ -41,10 +29,6 @@ app.get("/" ,(req ,res,next)=>{
     next();
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 9ccf282 (hi)
 app.use(express.static("upload"));
 //set template engine
 app.set('view engine', 'ejs');
